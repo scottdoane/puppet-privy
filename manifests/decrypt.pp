10bd25bac95a06d17d252b692c5caf1553fcbecb
Scott Doane <sdoane@austin.utexas.edu>
2015-11-23 10:38:24 -0600

    Updating variable names.

--- manifests/decrypt.pp
+++ manifests/decrypt.pp
@@ -1,14 +1,14 @@
 # == Define: privy
 #
 define privy::decrypt (
-  $encrypted_target_path = $title,
-  $decrypted_target_path,
-  $decription_key_path,
+  $encrypted_file_path = $title,
+  $decrypted_file_path,
+  $decryption_key_path,
   ) {
 
   Exec { path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'}
-  exec { "Decrypting $encrypted_target_path to $decrypted_target_path":
-    command => "openssl smime -decrypt -in $encrypted_target_path -inform DER -inkey $decription_key_path -out $decrypted_target_path",
-    creates => $decrypted_target_path
+  exec { "Decrypting $encrypted_file_path to $decrypted_file_path":
+    command => "openssl smime -decrypt -in $encrypted_file_path -inform DER -inkey $decryption_key_path -out $decrypted_file_path",
+    creates => $decrypted_file_path
   }
 }
